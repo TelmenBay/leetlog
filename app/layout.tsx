aa8ce7ab92bbe6e37e05dffa75d0e4d08149819c
TelmenBay <bayarakh@gmail.com>
2026-01-16 20:01:30 -0500

    feat: add landing page with timer and GitHub star counter

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "LeetLog",
+  description: "Your personal journal for mastering algorithms. Log problems, track progress, and stay consistent with your technical interview prep.",
+  icons: {
+    icon: '/icon_LL..svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
